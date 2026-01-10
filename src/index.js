b7c704f6efb555238311623be37f4d18bf3c2349
Thushani Wanigasinghe <116636766+thushaniwanigasinghe@users.noreply.github.com>
2026-01-10 11:21:50 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,32 +9,28 @@ import userRouter from "./routes/userRoutes.js";
 import absenceRouter from "./routes/absenceRoutes.js";
 import reliefAssignmentRouter from "./routes/reliefAssignmentRoutes.js";
 import announcementRoutes from "./routes/announcements.js";
+import timetableRoutes from "./routes/timetableRoutes.js";
+import attendanceRouter from "./routes/attendanceRoutes.js";
 import teacherSettingsRouter from "./routes/teacherSettingsRoutes.js";
 import adminSettingsRoutes from "./routes/adminSettingsRoutes.js";
-import reliefDutyRouter from "./routes/reliefDutyRoutes.js";
 
-// Global instances
+
+
 const app = express();
-const API_URL = process.env.VITE_API_URL;
-const PORT =
-  process.env.PORT || (API_URL.includes(":") ? API_URL.split(":").pop() : 3301);
+const PORT = process.env.PORT || 3301;
 connectDB();
 
-//  Middleware
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+// Common Middleware
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 
 // Base route
 app.get("/", (req, res) => {
-  res
-    .status(200)
-    .json({ success: { message: "Welcome to the TeachGrid Server!" } });
+  res.json({ message: "Server running" });
 });
 
 app.use("/api/auth", authRouter);
@@ -42,16 +38,18 @@ app.use("/api/user", userRouter);
 app.use("/api/absence", absenceRouter);
 app.use("/api/relief", reliefAssignmentRouter);
 app.use("/api/announcements", announcementRoutes);
+app.use("/api/timetable", timetableRoutes);
+app.use('/api/attendance', attendanceRouter);
 app.use("/api/teachers", teacherSettingsRouter);
 app.use("/api/admin", adminSettingsRoutes);
-app.use("/api/relief-duty", reliefDutyRouter);
+
 
 // Error route
 app.use((req, res) => {
   res.status(404).json({ error: { message: "not found on this server!" } });
 });
 
-// Initialize the server listener
+// Initialize the connection
 app.listen(PORT, () => {
   console.log(`Server is running ${PORT}`);
 });
