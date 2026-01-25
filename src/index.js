c4d3514cf3084d0b6187d31b2988bf4d97766db9
Safwan <140247051+Mrghost077@users.noreply.github.com>
2026-01-25 12:14:55 +0530

    Update server listen address to '0.0.0.0'

--- src/index.js
+++ src/index.js
@@ -62,6 +62,6 @@ app.use((req, res) => {
   res.status(404).json({ message: "Not Found" });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
 });
